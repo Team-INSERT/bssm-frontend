ffd7f085dbb230d0703e07e3eac2fc72d39312b2
Ubinquitous <ubinquitous1@gmail.com>
2023-06-27 11:07:12 +0900

    chore(apis): localStorage 기본 세팅

---
+++ src/apis/storage/index.ts
@@ -0,0 +1,17 @@
+type LocalStorageKey = "access_token" | "refresh_token" | "autoComplete";
+
+export default class Storage {
+  static getItem(key: LocalStorageKey) {
+    return typeof window !== "undefined" ? localStorage.getItem(key) : null;
+  }
+
+  static setItem(key: LocalStorageKey, value: string) {
+    if (typeof window === "undefined") return;
+    localStorage.setItem(key, value);
+  }
+
+  static delItem(key: LocalStorageKey) {
+    if (typeof window === "undefined") return;
+    localStorage.removeItem(key);
+  }
+}
