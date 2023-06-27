b46978a322146b3df2c547897424ea8a8983f61a
Ubinquitous <ubinquitous1@gmail.com>
2023-06-27 11:17:41 +0900

    chore(styles): 폰트 제네레이터로 사용할 폰트 정의, 색 정의 및 css 기본 세팅

---
+++ src/styles/color.ts
@@ -0,0 +1,17 @@
+const color = {
+  black: "#000000",
+  white: "#FFFFFF",
+  orange: "#FF763B",
+  red: "#F44336",
+
+  primary_red: "#E54F5A",
+  primary_blue: "#73AEE3",
+  primary_yellow: "#FEBC56",
+  primary_mint: "#27C3BC",
+
+  tertiary: "#303441",
+  container: "#F9FAFF",
+  content: "#D9D9D9",
+};
+
+export default color;
