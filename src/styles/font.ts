43cee25bbe7594dd0a35fb18e3b619239226aff7
Ubinquitous <ubinquitous1@gmail.com>
2023-06-28 23:17:27 +0900

    chore(font): sans-serif 정의

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -4,7 +4,7 @@ const fontGenerator = (
   lineHeight: number,
   letterSpacing: number,
 ) => `
-    font-family: 'Pretendard';
+    font-family: 'Pretendard', sans-serif;
     font-weight: ${weight};
     font-size: ${size}rem;
     line-height: ${lineHeight}%;
