669492a9fc619e9af13839146e19f6c785a80140
Ubinquitous <ubinquitous1@gmail.com>
2023-06-28 23:32:05 +0900

    chore(package): 스타일드 컴포넌트에 필요한 바벨 플러그인 정의

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import "@/styles/globals.css";
 import Provider from "@/utils/helpers/provider.helper";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "BSM",
@@ -16,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Provider>{children}</Provider>
       </body>
     </html>
