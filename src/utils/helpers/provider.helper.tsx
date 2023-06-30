e160d7e7225b0bb06f52260edc6fbfb5be76b837
Ubinquitous <ubinquitous1@gmail.com>
2023-06-30 16:51:11 +0900

    refactor(provider): 쿼리 클라이언트, view provider 분리

--- src/utils/helpers/provider.helper.tsx
+++ src/utils/helpers/provider.helper.tsx
@@ -3,43 +3,23 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import React, { PropsWithChildren } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
 import { ToastContainer, toast } from "react-toastify";
 import { RecoilRoot } from "recoil";
-import styled from "styled-components";
 import Column from "@/components/Flex/Column";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      suspense: false,
-    },
-  },
-});
-
-const CustomToastContainer = styled(ToastContainer)`
-  .Toastify__toast {
-    color: black;
-    font-size: 14px;
-  }
-`;
+import ReactQueryProvider from "./reactQueryProvider.helper";
 
 const Provider = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <ReactQueryProvider>
       <RecoilRoot>
-        <CustomToastContainer
-          autoClose={1000}
-          position={toast.POSITION.TOP_RIGHT}
-        />
+        <ToastContainer autoClose={1000} position={toast.POSITION.TOP_RIGHT} />
         <Column gap="6vh">
           <Header />
           {children}
           <Footer />
         </Column>
       </RecoilRoot>
-    </QueryClientProvider>
+    </ReactQueryProvider>
   );
 };
 
