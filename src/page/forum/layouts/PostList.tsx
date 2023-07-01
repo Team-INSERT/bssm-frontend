8cdaa7c566a0ac2611fe21815f19087f409d4fe3
Ubinquitous <ubinquitous1@gmail.com>
2023-07-01 17:15:43 +0900

    feat(forum list): 게시판 글 목록 컴포넌트 정의

---
+++ src/page/forum/layouts/PostList.tsx
@@ -0,0 +1,15 @@
+import Column from "@/components/Flex/Column";
+import React from "react";
+import PostListItem from "./PostListItem";
+
+const PostList = () => {
+  return (
+    <Column gap="8px">
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((key) => (
+        <PostListItem key={key} />
+      ))}
+    </Column>
+  );
+};
+
+export default PostList;
