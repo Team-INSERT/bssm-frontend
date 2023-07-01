4eaaedb9073cd9971e7bb6da12ed3f20f0e72035
Ubinquitous <ubinquitous1@gmail.com>
2023-07-01 20:19:43 +0900

    refactor(forum key): forum constant 카테고리 추가

---
+++ src/utils/constants/forum.constant.ts
@@ -0,0 +1,32 @@
+const forum = {
+  free: {
+    name: "자유",
+    type: "free",
+  },
+  student: {
+    name: "학생",
+    type: "student",
+  },
+  name: "forum",
+  type: "radio",
+  category: {
+    all: {
+      name: "전체",
+      type: "all",
+    },
+    complain: {
+      name: "불만",
+      type: "complain",
+    },
+    humor: {
+      name: "유머",
+      type: "humor",
+    },
+    infomation: {
+      name: "정보",
+      type: "infomation",
+    },
+  },
+};
+
+export default forum;
