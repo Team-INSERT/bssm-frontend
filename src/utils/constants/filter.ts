d8a7fa0fb6f6eb1ddeabf39585ee1dcd744360ff
Ubinquitous <ubinquitous1@gmail.com>
2023-07-01 17:16:55 +0900

    chore(contant): filter type constant key로 지정

---
+++ src/utils/constants/filter.ts
@@ -0,0 +1,14 @@
+const filter = {
+  free: {
+    name: "자유",
+    type: "free",
+  },
+  student: {
+    name: "학생",
+    type: "student",
+  },
+  name: "filter",
+  type: "radio",
+};
+
+export default filter;
