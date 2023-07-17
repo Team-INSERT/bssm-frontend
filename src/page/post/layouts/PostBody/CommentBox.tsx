81371780a11b720a0d2cbbd203d04a017e21690e
Ubinquitous <ubinquitous1@gmail.com>
2023-07-17 18:59:31 +0900

    chore(comment box): 댓글 컴포넌트 분리 및 퍼블리싱

---
+++ src/page/post/layouts/PostBody/CommentBox.tsx
@@ -0,0 +1,81 @@
+import Icon from "@/components/Icon";
+import color from "@/styles/color";
+import React from "react";
+import styled from "styled-components";
+import Emoji from "@/page/post/assets/Emoji.svg";
+import { font } from "@/styles/font";
+import AnonymousBox from "./AnonymousBox";
+
+const CommentBox = () => {
+  return (
+    <Container>
+      <CommentCount>124</CommentCount>
+      <CommentWriteBox>
+        <CommentTextArea />
+        <CommentToolBox>
+          <Icon src={Emoji} alt="emoticon" width={20} height={20} />
+          <AnonymousBox />
+          <CommentUploadButton />
+        </CommentToolBox>
+      </CommentWriteBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  border-top: 1.5px solid ${color.on_tertiary};
+  padding: 12px 0;
+  gap: 8px;
+`;
+
+const CommentCount = styled.h1`
+  ${font.H5};
+
+  &:before {
+    content: "댓글 ";
+  }
+
+  &:after {
+    content: "개";
+  }
+`;
+
+const CommentWriteBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+const CommentTextArea = styled.textarea`
+  width: 100%;
+  height: 100px;
+  border: 2px solid ${color.on_tertiary};
+  padding: 12px;
+  ${font.p3};
+`;
+
+const CommentToolBox = styled.div`
+  display: flex;
+  gap: 16px;
+  align-items: center;
+  margin-left: auto;
+`;
+
+const CommentUploadButton = styled.button`
+  width: 70px;
+  height: 32px;
+  background-color: ${color.primary_blue};
+  color: white;
+  border-radius: 3px;
+  ${font.btn3};
+
+  &:after {
+    content: "작성";
+  }
+`;
+
+export default CommentBox;
