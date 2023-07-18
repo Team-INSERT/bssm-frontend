1dc5430ddba05b608b0f13eab74121b8f0ad0f96
Ubinquitous <ubinquitous1@gmail.com>
2023-07-18 16:24:37 +0900

    chore(data): asset/data로 category data 파일 이동

---
+++ src/global/assets/data/categories.ts
@@ -0,0 +1,22 @@
+import forum from "@/page/forum/constants/forum.constant";
+
+const categories = [
+  {
+    name: forum.category.all.name,
+    type: forum.category.all.type,
+  },
+  {
+    name: forum.category.complain.name,
+    type: forum.category.complain.type,
+  },
+  {
+    name: forum.category.humor.name,
+    type: forum.category.humor.type,
+  },
+  {
+    name: forum.category.infomation.name,
+    type: forum.category.infomation.type,
+  },
+];
+
+export default categories;
