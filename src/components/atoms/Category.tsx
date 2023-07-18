ad7e84f36a668e0f9d4a29cd0f62f2b241050e43
Ubinquitous <ubinquitous1@gmail.com>
2023-07-18 16:24:11 +0900

    feat(atoms): category atoms 컴포넌트로 분리

---
+++ src/components/atoms/Category.tsx
@@ -0,0 +1,50 @@
+import color from "@/styles/color";
+import { font } from "@/styles/font";
+import React from "react";
+import styled, { css } from "styled-components";
+import Row from "../Flex/Row";
+
+interface ICategoryProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  id?: string;
+  label?: string;
+  checked: boolean;
+}
+
+const Category = ({ id, label, checked, ...props }: ICategoryProps) => {
+  return (
+    <Row>
+      <StyledCategory id={id} type="radio" {...props} />
+      <StyledLabel htmlFor={id} checked={checked}>
+        {label}
+      </StyledLabel>
+    </Row>
+  );
+};
+
+const StyledCategory = styled.input`
+  display: none;
+`;
+
+const StyledLabel = styled.label<{ checked: boolean }>`
+  border: none;
+  width: 64px;
+  height: 26px;
+  border-radius: 999px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 10px 0 rgba(144, 144, 144, 0.1);
+  ${font.btn3};
+  ${({ checked }) =>
+    checked
+      ? css`
+          background-color: ${color.primary_blue};
+          color: ${color.white};
+        `
+      : css`
+          background-color: ${color.white};
+          color: ${color.gray};
+        `}
+`;
+
+export default Category;
