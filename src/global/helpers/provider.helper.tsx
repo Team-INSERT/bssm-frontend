27c5aafa37cebf7ce702b51371ccebcb37907ff1
Ubinquitous <ubinquitous1@gmail.com>
2023-07-18 16:24:48 +0900

    chore(style): components 폴더 common, atoms로 분리

--- src/global/helpers/provider.helper.tsx
+++ src/global/helpers/provider.helper.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
 import React, { PropsWithChildren } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import { RecoilRoot } from "recoil";
