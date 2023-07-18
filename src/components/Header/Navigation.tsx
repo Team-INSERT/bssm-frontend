735389050f69616bb1793f79c68526241192faad
Ubinquitous <ubinquitous1@gmail.com>
2023-07-18 14:30:55 +0900

    refactor(header): 헤더 개편

--- src/components/Header/Navigation.tsx
+++ src/components/Header/Navigation.tsx
@@ -1,21 +1,43 @@
 import color from "@/styles/color";
 import { font } from "@/styles/font";
+import Link from "next/link";
 import React from "react";
 import styled from "styled-components";
 
-const navigation = [
-  "마이스터역량인증제",
-  "시간표",
-  "급식표",
-  "게시판",
-  "대나무숲",
+const navigations = [
+  {
+    name: "인증제",
+    href: "/meister",
+  },
+  {
+    name: "시간표",
+    href: "/timetable",
+  },
+  {
+    name: "급식표",
+    href: "/meal",
+  },
+  {
+    name: "일정표",
+    href: "/schedule",
+  },
+  {
+    name: "게시판",
+    href: "/forum",
+  },
+  {
+    name: "분실물 찾기",
+    href: "/lostfound",
+  },
 ];
 
 const Navigation = () => {
   return (
     <NavigationList>
-      {navigation.map((title) => (
-        <NavigationListItem key={title}>{title}</NavigationListItem>
+      {navigations.map((navigation) => (
+        <NavigationListItem key={navigation.href} href={navigation.href}>
+          {navigation.name}
+        </NavigationListItem>
       ))}
     </NavigationList>
   );
@@ -27,7 +49,7 @@ const NavigationList = styled.ul`
   gap: 5%;
 `;
 
-const NavigationListItem = styled.li`
+const NavigationListItem = styled(Link)`
   font-size: ${font.H5};
   color: ${color.black};
   cursor: pointer;
