2497398f810a02a119b00e8e12f8fdd582916460
Ubinquitous <ubinquitous1@gmail.com>
2023-07-18 19:26:29 +0900

    refactor(imageWithFallback): onErrorDelete 속성 추가

--- src/components/atoms/ImageWithFallback.tsx
+++ src/components/atoms/ImageWithFallback.tsx
@@ -1,32 +1,45 @@
 import Image, { StaticImageData } from "next/image";
 import { useState } from "react";
+import styled, { css } from "styled-components";
 
 interface ImageWithFallbackProps {
   src: string;
   fallbackSrc: StaticImageData | string;
   alt: string;
-  width?: number;
-  height?: number;
+  size?: string;
+  onErrorDelete?: boolean;
 }
 
 const ImageWithFallback = ({
   src,
   fallbackSrc,
   alt,
-  width,
-  height,
+  size,
+  onErrorDelete,
 }: ImageWithFallbackProps) => {
   const [imgSrc, setImgSrc] = useState<StaticImageData | string>(src);
 
   return (
-    <Image
+    <StyledImage
       src={imgSrc}
       alt={alt}
+      size={size}
+      onErrorDelete={onErrorDelete}
       onError={() => setImgSrc(fallbackSrc)}
-      width={width}
-      height={height}
+      width={999}
+      height={999}
     />
   );
 };
+
+const StyledImage = styled(Image)<{ size?: string; onErrorDelete?: boolean }>`
+  width: ${({ size }) => size};
+  height: auto;
+  ${({ onErrorDelete }) =>
+    onErrorDelete &&
+    css`
+      display: none;
+    `}
+`;
 
 export default ImageWithFallback;
