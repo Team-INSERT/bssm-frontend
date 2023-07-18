d853642ec29d4792aeefea61c1e2d0fb8b966257
Ubinquitous <ubinquitous1@gmail.com>
2023-07-18 16:23:03 +0900

    chore(router): post 라우터로 한번에 묶어 처리

---
+++ src/app/post/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import ForumPage from "@/page/forum";
+
+const Post = () => {
+  return <ForumPage />;
+};
+
+export default Post;
---
+++ src/app/post/write/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import WritePage from "@/page/write";
+
+const Write = () => {
+  return <WritePage />;
+};
+
+export default Write;
