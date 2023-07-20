a2d95a2e4e441284bb4758b3d6770968ecfdeb3e
Ubinquitous <ubinquitous1@gmail.com>
2023-07-20 18:09:41 +0900

    chore(aside): user api 연결 및 값 제공, 조건부 렌더링

--- src/components/common/Aside/index.tsx
+++ src/components/common/Aside/index.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import Row from "@/components/Flex/Row";
+import useUser from "@/hooks/useUser";
 import InfomationBox from "./InfomationBox";
 import MeisterBox from "./MeisterBox";
 import JoinCheckBox from "./JoinCheckBox";
 
 const Aside = () => {
+  const { user, isLogined } = useUser();
+
   return (
     <Container>
-      <InfomationBox />
-      <Row gap="6px">
-        <MeisterBox />
-        <JoinCheckBox />
-      </Row>
+      <InfomationBox user={user} isLogined={isLogined} />
+      {isLogined && (
+        <Row gap="6px">
+          <MeisterBox />
+          <JoinCheckBox />
+        </Row>
+      )}
     </Container>
   );
 };
