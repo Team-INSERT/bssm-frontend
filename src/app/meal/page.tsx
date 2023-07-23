10e82451d486be69f3d1773a75e13af0182772d4
5ewon06 <gwonsewon03@gmail.com>
2023-07-23 20:32:38 +0900

    feat(meal): 급식 페이지 추가

---
+++ src/app/meal/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import MealPage from "@/page/meal";
+import React from "react";
+
+const Meal = () => {
+  return <MealPage />;
+};
+
+export default Meal;
---
+++ src/page/meal/index.tsx
@@ -0,0 +1,29 @@
+import Aside from "@/components/Aside";
+import React from "react";
+import styled from "styled-components";
+import Meal from "./layouts/Meal";
+
+const MealPage = () => {
+  return (
+    <Layout>
+      <Container>
+        <Meal />
+        <Aside />
+      </Container>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  width: 76%;
+  height: 100vh;
+  display: flex;
+`;
+
+export default MealPage;
