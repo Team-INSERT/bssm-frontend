858ef830ac74ed999a2ef60188180d34ed4db752
Ubinquitous <ubinquitous1@gmail.com>
2023-07-29 18:14:20 +0900

    feat(CustomEditor): 클릭 시 모달나오는 기능 구현

--- src/components/atoms/CustomEditor.tsx
+++ src/components/atoms/CustomEditor.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { Editor as TinymcEditor } from "@tinymce/tinymce-react";
 import { font } from "@/styles/font";
+import Kissing from "@/global/assets/svgs/emojis/Kissing";
+import styled from "styled-components";
+import EmojiModal from "../common/Modal/EmojiModal";
 
 const CustomEditor = () => {
   const [content, setContent] = React.useState("");
+  const [isOpenEmojiModal, setIsOpenEmojiModal] = React.useState(false);
+
+  const handleEmojiButtonClick = () => {
+    setIsOpenEmojiModal(true);
+  };
 
   const imagesUploadHandler = async (blobInfo: any): Promise<string> => {
     return new Promise(() => {
@@ -13,53 +21,60 @@ const CustomEditor = () => {
   };
 
   return (
-    <TinymcEditor
-      init={{
-        language: "ko_KR",
-        height: "300px",
-        menubar: false,
-        content_css: "white",
-        plugins: [
-          "code",
-          "autolink",
-          "lists",
-          "link",
-          "image",
-          "charmap",
-          "preview",
-          "anchor",
-          "searchreplace",
-          "visualblocks",
-          "media",
-          "table",
-          "wordcount",
-          "codesample",
-        ],
-        toolbar:
-          "undo redo codesample | bold italic | alignleft alignright aligncenter alignjustify | emoticon image media | preview code",
-        codesample_global_prismjs: true,
-        setup: (tinymceEditor) => {
-          tinymceEditor.ui.registry.addButton("emoticon", {
-            text: "이모티콘",
-            onAction: () => {
-              console.log(1);
-              //   openModal("emoticon");
-            },
-          });
-        },
-        relative_urls: false,
-        convert_urls: false,
-        extended_valid_elements: "img[src|class|alt|e_id|e_idx|e_type]",
-        images_upload_handler: imagesUploadHandler,
-        init_instance_callback: (e) => {
-          const css = document.createElement("style");
-          css.innerHTML = StyledCSS;
-          e.contentDocument.head.appendChild(css);
-        },
-      }}
-      value={content}
-      onEditorChange={(props) => setContent(props)}
-    />
+    <Container>
+      {isOpenEmojiModal && (
+        <>
+          <EmojiBox>
+            <EmojiModal />
+          </EmojiBox>
+          <ModalBackground onClick={() => setIsOpenEmojiModal(false)} />
+        </>
+      )}
+      <TinymcEditor
+        init={{
+          language: "ko_KR",
+          height: "300px",
+          menubar: false,
+          content_css: "white",
+          plugins: [
+            "code",
+            "autolink",
+            "lists",
+            "link",
+            "image",
+            "charmap",
+            "preview",
+            "anchor",
+            "searchreplace",
+            "visualblocks",
+            "media",
+            "table",
+            "wordcount",
+            "codesample",
+          ],
+          toolbar:
+            "undo redo codesample | bold italic | alignleft alignright aligncenter alignjustify | emoticon image media | preview code",
+          codesample_global_prismjs: true,
+          setup: (tinymceEditor) => {
+            tinymceEditor.ui.registry.addButton("emoticon", {
+              icon: "emoji",
+              onAction: handleEmojiButtonClick,
+            });
+          },
+          relative_urls: false,
+          convert_urls: false,
+          extended_valid_elements: "img[src|class|alt|e_id|e_idx|e_type]",
+          images_upload_handler: imagesUploadHandler,
+          init_instance_callback: (e) => {
+            const css = document.createElement("style");
+            css.innerHTML = StyledCSS;
+            e.contentDocument.head.appendChild(css);
+          },
+        }}
+        value={content}
+        onEditorChange={(props) => setContent(props)}
+      />
+    </Container>
   );
 };
 
@@ -84,6 +99,25 @@ const StyledCSS = `
   body {
     padding: 18px !important;
   }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;
+
+const EmojiBox = styled.div`
+  position: absolute;
+  left: 54%;
+  bottom: 100%;
+  z-index: 10;
+`;
+
+const ModalBackground = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
 `;
 
 export default CustomEditor;
