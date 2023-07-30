a316075763e431de7d96901f3bf0374c40d12f06
Ubinquitous <ubinquitous1@gmail.com>
2023-07-30 22:16:50 +0900

    chore(style): border-radius 추가

--- src/components/common/Modal/SettingModal/index.tsx
+++ src/components/common/Modal/SettingModal/index.tsx
@@ -1,16 +1,25 @@
 import color from "@/styles/color";
 import React from "react";
 import styled from "styled-components";
+import SettingHeader from "./SettingHeader";
+import SettingBody from "./SettingBody";
 
 const SettingModal = () => {
-  return <Container />;
+  return (
+    <Container>
+      <SettingHeader />
+      <SettingBody />
+    </Container>
+  );
 };
 
 const Container = styled.div`
-  width: 30vw;
-  height: 40vh;
+  width: 40vw;
+  height: 90vh;
+  overflow-y: scroll;
   background-color: ${color.white};
   box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.2);
+  border-radius: 8px;
 `;
 
 export default SettingModal;
