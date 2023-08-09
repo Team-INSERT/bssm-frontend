94152811e9113749bdfb89740e9730c09f877751
Ubinquitous <ubinquitous1@gmail.com>
2023-08-09 16:11:46 +0900

    chore(msw): msw 설정 해제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import initMockAPI from "@/mocks";
 import HomePage from "@/page/home";
-
-if (process.env.NODE_ENV === "development") initMockAPI();
 
 const Home = () => {
   return <HomePage />;
