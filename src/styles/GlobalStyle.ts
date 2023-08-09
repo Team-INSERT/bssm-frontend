01b238f82f4914e019662931c2dce39f3b6ec5a9
Ubinquitous <ubinquitous1@gmail.com>
2023-08-09 16:14:19 +0900

    fix(GlobalStyle): GlobalStyle 내 import문 삭제

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -3,8 +3,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @import url("https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css");
-
   * {
     margin: 0;
     padding: 0;
