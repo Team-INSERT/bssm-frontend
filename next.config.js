c365943ab9a089ab15b43f045a3a750562712e31
Ubinquitous <ubinquitous1@gmail.com>
2023-08-12 14:43:07 +0900

    fix(nextConfig): 이미지 도메인 env 사용, 테스트서버 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["bssm.kro.kr"],
+    domains: [process.env.NEXT_PUBLIC_DOMAIN, process.env.NEXT_PUBLIC_TEST_DOMAIN],
   },
 };
 
