63fa6ee8ffe5cb289298a12f5f7fb2623b0d20ad
Ubinquitous <ubinquitous1@gmail.com>
2023-08-12 14:46:02 +0900

    chore(helper): 레이아웃 프로바이더 분리

---
+++ src/helpers/layoutProvider.helper.tsx
@@ -0,0 +1,22 @@
+import { Column } from "@/components/Flex";
+import { Footer, Header, Modal } from "@/components/common";
+import GlobalStyle from "@/styles/GlobalStyle";
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+
+const LayoutProvider = ({ children }: React.PropsWithChildren) => {
+  return (
+    <>
+      <ToastContainer autoClose={1000} position={toast.POSITION.TOP_RIGHT} />
+      <GlobalStyle />
+      <Modal />
+      <Column gap="6vh">
+        <Header />
+        {children}
+        <Footer />
+      </Column>
+    </>
+  );
+};
+
+export default LayoutProvider;
