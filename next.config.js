f97946c5099c9a44510137e7e0554cf8a4d45e4f
Ubinquitous <ubinquitous1@gmail.com>
2023-08-13 07:44:22 +0900

    chore(domains): auth domain 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [process.env.NEXT_PUBLIC_DOMAIN, process.env.NEXT_PUBLIC_TEST_DOMAIN],
+    domains: [
+      process.env.NEXT_PUBLIC_DOMAIN,
+      process.env.NEXT_PUBLIC_TEST_DOMAIN,
+      process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    ],
   },
 };
 
