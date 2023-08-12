70e9ef4de1c450690eac7a2c6f9f22e01668701d
Ubinquitous <ubinquitous1@gmail.com>
2023-08-13 07:46:25 +0900

    feat(constant): storageKey 추가

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,3 +8,4 @@ export { default as ROUTER } from "./router.constant";
 export { default as SERVICE } from "./service.constant";
 export { default as TOKEN } from "./token.constant";
 export { default as USER } from "./user.constant";
+export { default as STORAGE_KEY } from "./storageKey.constant";
