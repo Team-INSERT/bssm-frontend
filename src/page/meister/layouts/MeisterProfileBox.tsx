1501a9433b739b335a329700dc571d17ae5b3c99
Ubinquitous <ubinquitous1@gmail.com>
2023-08-18 01:30:49 +0900

    chore(MeisterProfiileBox): 마역량 프로필 컴포넌트 추가

---
+++ src/page/meister/layouts/MeisterProfileBox.tsx
@@ -0,0 +1,127 @@
+import { defaultProfile } from "@/assets/images";
+import { Column, Row } from "@/components/Flex";
+import { ImageWithFallback } from "@/components/atoms";
+import { color, font } from "@/styles";
+import React from "react";
+import styled from "styled-components";
+
+const MeisterProfileBox = () => {
+  return (
+    <Container>
+      <ImageWithFallback
+        src="https://auth.bssm.kro.kr/_next/image?url=https%3A%2F%2Fauth.bssm.kro.kr%2Fresource%2Fuser%2Fprofile%2F45.png&w=256&q=75"
+        fallbackSrc={defaultProfile}
+        alt="profile"
+        width={50}
+        height={50}
+        rounded
+      />
+      <Column>
+        <Row alignItems="center" gap="5px">
+          <Department>소프트웨어</Department>
+          <Grade>2</Grade>
+          <ClassNo>2</ClassNo>
+          <StudentNo>10</StudentNo>
+          <Name>박우빈</Name>
+        </Row>
+        <MeisterPoint>135.2</MeisterPoint>
+        <Row alignItems="center" gap="8px">
+          <CreditPoint>48</CreditPoint>
+          <Separator />
+          <DemeritPoint>22</DemeritPoint>
+        </Row>
+      </Column>
+      <MyPageButton />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  padding: 14px 24px;
+  background-color: ${color.white};
+  display: flex;
+  align-items: center;
+  gap: 18px;
+`;
+
+const InfomationText = styled.span`
+  ${font.H6};
+  color: ${color.black};
+`;
+
+const Department = styled(InfomationText)`
+  &:after {
+    content: "개발과";
+  }
+`;
+
+const Grade = styled(InfomationText)`
+  &:after {
+    content: "학년";
+  }
+`;
+
+const ClassNo = styled(InfomationText)`
+  &:after {
+    content: "반";
+  }
+`;
+
+const StudentNo = styled(InfomationText)`
+  &:after {
+    content: "번";
+  }
+`;
+
+const Name = styled(InfomationText)``;
+
+const RewardPointText = styled.span`
+  ${font.p3};
+  line-height: 130%;
+  color: ${color.gray};
+
+  &:after {
+    content: "점";
+  }
+`;
+
+const MeisterPoint = styled(RewardPointText)`
+  &:before {
+    content: "마이스터역량인증제 점수 ㆍ ";
+  }
+`;
+
+const CreditPoint = styled(RewardPointText)`
+  &:before {
+    content: "상점 ㆍ ";
+  }
+`;
+
+const Separator = styled.div`
+  width: 1.5px;
+  background-color: ${color.on_tertiary};
+  height: 12px;
+`;
+
+const DemeritPoint = styled(RewardPointText)`
+  &:before {
+    content: "벌점 ㆍ ";
+  }
+`;
+
+const MyPageButton = styled.div`
+  padding: 4px 10px;
+  border-radius: 4px;
+  margin-left: auto;
+  background-color: ${color.primary_blue};
+  ${font.btn3};
+  color: ${color.white};
+  cursor: pointer;
+
+  &:after {
+    content: "내 정보";
+  }
+`;
+
+export default MeisterProfileBox;
