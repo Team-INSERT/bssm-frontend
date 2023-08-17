8a18292f7d18364717bcfef158d2d96326fe8394
Ubinquitous <ubinquitous1@gmail.com>
2023-08-18 01:26:44 +0900

    chore(icon): 레이더 차트 아이콘 추가

---
+++ src/assets/icons/DistributionIcon.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const DistributionIcon = () => {
+  return (
+    <svg
+      width="27"
+      height="27"
+      viewBox="0 0 27 27"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.67309 15.2707L7.24222 7.09598L15.6228 7.09598L22.6184 13.0803L17.8311 19.1659L10.5349 22.0678L5.67309 15.2707Z"
+        fill="#F8EEE6"
+        fillOpacity="0.5"
+        stroke="#F3DAEC"
+        strokeWidth="2"
+      />
+      <path
+        d="M4.94863 12.9523L10.1108 5.49777L20.522 9.26081L20.7492 12.7586L18.2199 20.6836L8.78822 20.2192L4.94863 12.9523Z"
+        fill="#C8D3F8"
+        fillOpacity="0.5"
+        stroke="#C0CCED"
+        strokeWidth="2"
+      />
+      <path
+        d="M9.50133 9.24329L16.8744 5.45424L21.79 16.1046L17.8985 19.7912L12.5736 23.3755L5.40478 18.3576L9.50133 9.24329Z"
+        fill="#FEBC56"
+        fillOpacity="0.3"
+      />
+      <path
+        d="M6.6561 18.0128L10.2699 9.97265L16.4036 6.8205L20.5813 15.8721L17.2711 19.0081L12.5846 22.1626L6.6561 18.0128Z"
+        stroke="#FEBC56"
+        strokeOpacity="0.4"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+};
+
+export default DistributionIcon;
