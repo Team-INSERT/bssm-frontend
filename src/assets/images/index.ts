52033a902f05d01d8bf5213f605d3a3fcfa33d86
Ubinquitous <ubinquitous1@gmail.com>
2023-08-20 15:52:50 +0900

    chore(images): fluent emoji 추가

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,8 @@
 export { default as emoticon } from "./emoticon.png";
-export { default as emptyImage } from "./emptyImage.png";
+export { default as emptyImage } from "./empty_image.png";
 export { default as loading } from "./loading.gif";
 export { default as defaultProfile } from "./profile_default.png";
 export { default as QR } from "./QR.png";
+export { default as ShushingFace } from "./shushing_face.png";
+export { default as ThinkingFace } from "./thinking_face.png";
+export { default as HuggingFace } from "./hugging_face.png";
