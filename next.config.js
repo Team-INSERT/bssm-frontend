253aa8e0a7c7658e73a650caa3557ea491a9a399
Ubinquitous <ubinquitous1@gmail.com>
2023-08-20 15:55:00 +0900

    fix(lint): 이미지 도메인

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      process.env.NEXT_PUBLIC_DOMAIN,
-      process.env.NEXT_PUBLIC_TEST_DOMAIN,
-      process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    ],
-  },
+  // images: {
+  //   domains: [
+  //     process.env.NEXT_PUBLIC_DOMAIN,
+  //     process.env.NEXT_PUBLIC_TEST_DOMAIN,
+  //     process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
