eab73061954c7de7aa8dd5c78aad5e6b24808a07
Ubinquitous <ubinquitous1@gmail.com>
2023-08-22 08:36:50 +0900

    fix(token): cookie에서 Bearer 토큰으로 기획 변경

--- src/helpers/provider.helper.tsx
+++ src/helpers/provider.helper.tsx
@@ -2,18 +2,15 @@
 
 import React from "react";
 import { RecoilRoot } from "recoil";
-import { CookiesProvider } from "react-cookie";
 import ReactQueryProvider from "./reactQueryProvider.helper";
 import LayoutProvider from "./layoutProvider.helper";
 
 const Provider = ({ children }: React.PropsWithChildren) => {
   return (
     <ReactQueryProvider>
-      <CookiesProvider>
-        <RecoilRoot>
-          <LayoutProvider>{children}</LayoutProvider>
-        </RecoilRoot>
-      </CookiesProvider>
+      <RecoilRoot>
+        <LayoutProvider>{children}</LayoutProvider>
+      </RecoilRoot>
     </ReactQueryProvider>
   );
 };
