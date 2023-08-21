767ad5aacfbdc1878ef2a7552cbb9d485eae12ee
Ubinquitous <ubinquitous1@gmail.com>
2023-08-22 08:20:36 +0900

    feat(modal): BambooManageModal 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,3 +4,4 @@ export { default as Header } from "./Header";
 export { default as Modal } from "./Modal";
 export { default as SettingModal } from "./Modal/SettingModal";
 export { default as EmojiModal } from "./Modal/EmojiModal";
+export { default as BambooManageModal } from "./Modal/BambooManageModal";
