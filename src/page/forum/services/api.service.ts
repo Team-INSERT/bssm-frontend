b74f654310d5e1c68844dfe452acae381893c58a
Ubinquitous <ubinquitous1@gmail.com>
2023-08-30 17:08:34 +0900

    chore(gql): gql로 dto 변경, rest API 로직 삭제

--- src/page/forum/services/api.service.ts
+++
@@ -1,7 +0,0 @@
-import httpClient from "@/apis/httpClient";
-import { IPostListQuery } from "@/interfaces";
-
-export const getPostList = async (postConfig: IPostListQuery) => {
-  const { data } = await httpClient.post.getPostList(postConfig);
-  return data;
-};
