c58a8ecf706222130c799dc2f67b44ebbeb66e9a
Ubinquitous <ubinquitous1@gmail.com>
2023-08-30 17:05:44 +0900

    chore(profileUrl): URL관련 로직 삭제 ( 추후 hooks로 구현 추천 )

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,2 @@
-export { default as getProfileUrl } from "./getProfileUrl.helper";
 export { default as getUserRole } from "./getUserRole.helper";
 export { default as getStatusColor } from "./getStatusColor.helper";
