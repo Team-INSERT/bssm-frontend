b2290df469609478e6bd12ef3d0576df310a3e39
Ubinquitous <ubinquitous1@gmail.com>
2023-09-07 11:00:45 +0800

    fix(forum): 잔잔한 오류 수정 ( dto 수정 )

---
+++ src/page/timetable/services/query.service.ts
@@ -0,0 +1,10 @@
+import { KEY } from "@/constants";
+import { useQuery } from "react-query";
+import { ITimetable, IClassLevel } from "@/interfaces";
+import { getTimetable } from "./api.service";
+
+export const useTimetableListQuery = (classLevel: IClassLevel) => {
+  return useQuery<ITimetable>([KEY.TIMETABLE], async () =>
+    getTimetable(classLevel),
+  );
+};
