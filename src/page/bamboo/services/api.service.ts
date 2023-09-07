97d9822459c7918eeaf15462653157ee16b33f48
Ubinquitous <ubinquitous1@gmail.com>
2023-09-07 11:00:02 +0800

    chore(api): get, create, update, delete API 정의

--- src/page/bamboo/services/api.service.ts
+++ src/page/bamboo/services/api.service.ts
@@ -1,5 +1,30 @@
 import httpClient from "@/apis/httpClient";
 
-export const getBambooPost = async () => {
-  // const { data } = await httpClient.
+export const getBambooPostList = async () => {
+  const { data } = await httpClient.bamboo.get();
+  return data;
+};
+
+export const getBambooPendingPostList = async () => {
+  const { data } = await httpClient.bamboo.admin();
+  return data;
+};
+
+export const createBambooPost = async (content: string) => {
+  const { data } = await httpClient.bamboo.post({ content });
+  return data;
+};
+
+export const updateBambooPost = async (id: number) => {
+  const { data } = await httpClient.admin.putById(null, {
+    params: { id },
+  });
+  return data;
+};
+
+export const deleteBambooPost = async (id: number) => {
+  const { data } = await httpClient.admin.deleteById({
+    params: { id },
+  });
+  return data;
 };
