ef422146db98552d740793fd6881113764c51f64
Ubinquitous <ubinquitous1@gmail.com>
2023-09-26 17:11:45 +0900

    chore(helper): lost, found 분리

--- src/helpers/getCategory.helper.ts
+++ src/helpers/getCategory.helper.ts
@@ -3,7 +3,8 @@ import { PostCategoryType } from "@/types";
 const POSTNAME = {
   COMMON: "일반",
   CODE_REVIEW: "코드 리뷰",
-  LOST_FOUND: "분실물 찾기",
+  LOST: "분실했어요",
+  FOUND: "습득했어요",
   NOTICE: "공지사항",
   PROJECT: "팀원 모집",
 };
