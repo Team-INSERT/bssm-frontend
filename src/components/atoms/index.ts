ee8ef52f08a24c20ebf6852177f185063b3a4f78
Ubinquitous <ubinquitous1@gmail.com>
2023-09-27 08:34:10 +0900

    chore(CommentBox): atom에서 일반 컴포넌트로

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,6 +1,5 @@
 export { default as AnonymousBox } from "./AnonymousBox";
 export { default as Category } from "./Category";
-export { default as CommentBox } from "./CommentBox";
 export { default as CustomEditor } from "./CustomEditor";
 export { default as ImageWithFallback } from "./ImageWithFallback";
 export { default as Input } from "./Input";
