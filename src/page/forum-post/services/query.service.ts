49a1c2026a18673b311630f19229ed6f2bf90561
Ubinquitous <ubinquitous1@gmail.com>
2023-09-26 17:19:14 +0900

    chore(post): type없이 요청

--- src/page/forum-post/services/query.service.ts
+++ src/page/forum-post/services/query.service.ts
@@ -2,9 +2,9 @@ import { IPostQuery } from "@/interfaces";
 import { GET_POST } from "@/gql/post/queries";
 import { useQuery } from "@apollo/client";
 
-export const usePostQuery = ({ type, id }: IPostQuery) => {
-  const { data, ...queryRest } = useQuery(GET_POST({ type, id }), {
-    variables: { type, id },
+export const usePostQuery = ({ id }: IPostQuery) => {
+  const { data, ...queryRest } = useQuery(GET_POST({ id }), {
+    variables: { id },
   });
 
   return { post: data?.readOne, ...queryRest };
