2bf594dca0861d0809f81188956f08f80475abb0
Ubinquitous <ubinquitous1@gmail.com>
2023-09-27 08:37:33 +0900

    chore(mutation): create, update, delete mutation 추가

--- src/page/forum-post/services/mutation.service.ts
+++ src/page/forum-post/services/mutation.service.ts
@@ -1,14 +1,25 @@
-import { ROUTER } from "@/constants";
+import { KEY, ROUTER } from "@/constants";
 import { DELETE_POST } from "@/gql/post/queries";
-import { useApolloClient, useMutation } from "@apollo/client";
+import {
+  useApolloClient,
+  useMutation as useApolloMutation,
+} from "@apollo/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
+import {
+  createPostComment,
+  deletePostComment,
+  updateCommentLike,
+  updatePostComment,
+  updatePostLike,
+} from "./api.service";
 
 export const useDeletePostMutation = () => {
   const apolloClient = useApolloClient();
   const router = useRouter();
 
-  const mutations = useMutation(DELETE_POST, {
+  const mutations = useApolloMutation(DELETE_POST, {
     onCompleted: () => {
       apolloClient.cache.reset();
       toast.success("글이 삭제되었습니다!");
@@ -16,4 +27,67 @@ export const useDeletePostMutation = () => {
     },
   });
   return mutations;
+};
+
+interface IUseCreateCommentMutationProps {
+  id: string;
+  detail: string;
+}
+
+export const useUpdatePostLikeMutation = () => {
+  const apolloClient = useApolloClient();
+
+  return useMutation((id: string) => updatePostLike(id), {
+    onSuccess: () => {
+      apolloClient.cache.reset();
+    },
+  });
+};
+
+export const useCreatePostCommentMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (props: IUseCreateCommentMutationProps) => createPostComment(props),
+    {
+      onSuccess: () => {
+        toast.success("댓글을 작성했어요!");
+        queryClient.invalidateQueries([KEY.COMMENT]);
+      },
+    },
+  );
+};
+
+interface IUseUpdatePostCommentMutationProps {
+  id: number;
+  detail: string;
+}
+
+export const useUpdatePostCommentMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(
+    (comment: IUseUpdatePostCommentMutationProps) => updatePostComment(comment),
+    {
+      onSuccess: () => {
+        toast.success("댓글이 수정되었어요!");
+        queryClient.invalidateQueries([KEY.COMMENT]);
+      },
+    },
+  );
+};
+
+export const useDeletePostCommentMutation = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation((id: number) => deletePostComment(id), {
+    onSuccess: () => {
+      toast.success("댓글이 삭제되었어요.");
+      queryClient.invalidateQueries([KEY.COMMENT]);
+    },
+  });
+};
+
+export const useUpdateCommentLikeMutation = () => {
+  return useMutation((id: number) => updateCommentLike(id));
 };
