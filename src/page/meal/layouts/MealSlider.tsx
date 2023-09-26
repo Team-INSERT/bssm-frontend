4125edb5a9e2ee8bb8a8303173282a8d5c609dec
Ubinquitous <ubinquitous1@gmail.com>
2023-09-26 17:19:53 +0900

    chore(MealSlider): 개편 예정인 컴포넌트 삭제

--- src/page/meal/layouts/MealSlider.tsx
+++ src/page/meal/layouts/MealSlider.tsx
@@ -1,36 +1,17 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
 import styled from "styled-components";
-import MealListItem from "./MealListItem";
 
 const MealSlider = () => {
-  const settings = {
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 300,
-    swipe: true,
-    swipeToSlide: true,
-    vertical: true,
-    verticalSwiping: true,
-    beforeChange: (currentSlide: number, nextSlide: number) => {
-      console.log("before change", currentSlide, nextSlide);
-    },
-    afterChange: (currentSlide: number) => {
-      console.log("after change", currentSlide);
-    },
-  };
-
   return (
     <Container>
       <MealList>
-        <Slider {...settings}>
+        {/* <Slider {...settings}>
           {Array.from({ length: 10 }).map((_, i) => (
             <MealListItem key={i} />
           ))}
-        </Slider>
+        </Slider> */}
       </MealList>
     </Container>
   );
