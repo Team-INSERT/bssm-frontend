ca29d0f2f86d2f3c6eed407440782f2e8d2e9f45
Ubinquitous <ubinquitous1@gmail.com>
2023-09-26 19:55:05 +0900

    fix(refresh): 새로운 instance로 변경

--- src/apis/token/refresh.ts
+++ src/apis/token/refresh.ts
@@ -1,16 +1,16 @@
 import { TOKEN } from "@/constants/";
 import Storage from "@/apis/storage";
-import httpClient from "../httpClient";
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+});
 
 const refresh = async () => {
-  try {
-    const { data } = await httpClient.refresh.put({
-      refreshToken: `${Storage.getItem(TOKEN.REFRESH)}`,
-    });
-    Storage.setItem(TOKEN.ACCESS, data.accessToken);
-  } catch (err) {
-    Storage.clear();
-  }
+  const { data } = await instance.put("/api/auth/refresh/access", {
+    refreshToken: `${Storage.getItem(TOKEN.REFRESH)}`,
+  });
+  Storage.setItem(TOKEN.ACCESS, data.accessToken);
 };
 
 export default refresh;
