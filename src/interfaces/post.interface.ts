1869445c45fbd0a9692b8d07f14a1c12a82d0a79
Ubinquitous <ubinquitous1@gmail.com>
2023-09-26 17:14:29 +0900

    chore(post): 카테고리별 속성 인터페이스에 추가 정의

--- src/interfaces/post.interface.ts
+++ src/interfaces/post.interface.ts
@@ -1,18 +1,37 @@
 import { PostCategoryType } from "@/types";
 
 export default interface IPost {
-  id: number;
+  id: string;
+  title: string;
+  content: string;
   user: {
     id: number;
     nickName: string;
     profileImage: string;
   };
   category: PostCategoryType;
-  title: string;
   createdAt: string;
-  view: number;
   likeCount: number;
   commentCount: number;
-  content: string;
-  isMyLike: boolean;
+  doesLike: boolean;
+
+  // POST.PROJECT
+  startTime?: string;
+  endTime?: string;
+  field?: string;
+
+  // POST.CODE_REVIEW
+  prUrl?: string;
+  isFinished?: boolean;
+
+  // POST.LOST & POST.FOUND
+  lostThingImage?: string;
+  place?: string;
+  foundUser?: {
+    id: number;
+    nickName: string;
+  };
+
+  // POST.FOUND
+  keepingPlace?: string;
 }
