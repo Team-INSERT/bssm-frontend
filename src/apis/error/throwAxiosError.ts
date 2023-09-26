25828032a3dd7ac7dd724cb54fee77a6eb7b7870
Ubinquitous <ubinquitous1@gmail.com>
2023-09-26 19:45:47 +0900

    chore(axiosError): if문 수정

--- src/apis/error/throwAxiosError.ts
+++ src/apis/error/throwAxiosError.ts
@@ -1,20 +1,11 @@
 import { isAxiosError } from "axios";
-import { toast } from "react-toastify";
 
 const throwAxiosError = (err: unknown) => {
-  if (!isAxiosError(err))
-    return {
-      data: "",
-      status: "",
-      message: "",
-    };
+  if (isAxiosError(err) && err.response) {
+    const { code, status, message } = err.response.data;
 
-  const data = err?.response?.data;
-  const { code, status, message } = data;
-  console.log(err);
-  toast.error("오류가 발생했습니다.");
-
-  return { code, status, message };
+    return { code, status, message };
+  }
 };
 
 export default throwAxiosError;
