7a57767b59f2fab25f45bfda140172b3614a74e5
Ubinquitous <ubinquitous1@gmail.com>
2023-09-27 16:13:54 +0900

    ci(workflows): file server url 추가

--- next.config.js
+++ next.config.js
@@ -2,13 +2,14 @@ const removeImports = require("next-remove-imports")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = removeImports({
-  // images: {
-  //   domains: [
-  //     process.env.NEXT_PUBLIC_DOMAIN,
-  //     process.env.NEXT_PUBLIC_TEST_DOMAIN,
-  //     process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  //   ],
-  // },
+  images: {
+    domains: [
+      process.env.NEXT_PUBLIC_DOMAIN,
+      process.env.NEXT_PUBLIC_TEST_DOMAIN,
+      process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+      process.env.NEXT_PUBLIC_FILE_SERVER_URL,
+    ],
+  },
 });
 
 module.exports = nextConfig;
