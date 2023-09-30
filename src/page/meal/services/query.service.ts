54c2f21aed5f4abfb67e029637522adbe4f9f375
Ubinquitous <ubinquitous1@gmail.com>
2023-09-30 15:00:46 +0900

    chore(service): 급식 관련 query 정의

---
+++ src/page/meal/services/query.service.ts
@@ -0,0 +1,12 @@
+import { KEY } from "@/constants";
+import { useQuery } from "@tanstack/react-query";
+import { getMealList } from "./api.service";
+
+interface IUseMealQueryProps {
+  date: string;
+}
+
+export const useMealQuery = ({ date }: IUseMealQueryProps) => {
+  const { data, ...queryRest } = useQuery([KEY.MEAL], () => getMealList(date));
+  return { data, ...queryRest };
+};
