3e7495303e45060b6d25c5f2913fcffbbefc39f6
Ubinquitous <ubinquitous1@gmail.com>
2023-09-30 15:00:32 +0900

    chore(MealListItem): 급식 아이템 컴포넌트 정의

--- src/page/meal/layouts/MealListItem.tsx
+++ src/page/meal/layouts/MealListItem.tsx
@@ -1,56 +1,89 @@
-import { Row } from "@/components/Flex";
-import { color } from "@/styles";
 import React from "react";
 import styled from "styled-components";
+import { color, flex, font } from "@/styles";
+import { Row } from "@/components/Flex";
+import { getMealName } from "@/helpers";
+import BlinkerBox from "./BlinkerBox";
 
-const MealListItem = () => {
+interface IMealListItemProps {
+  mealName: string;
+  meal: {
+    content: string;
+    cal: number;
+  };
+}
+
+const MealListItem = ({ mealName, meal }: IMealListItemProps) => {
   return (
     <Container>
       <MealHeader>
-        <Row gap="8px" alignItems="center">
-          <Red />
-          <Yellow />
-          <Green />
+        <BlinkerBox />
+        <Row gap="4px">
+          <MealTime>{getMealName(mealName)}</MealTime>
+          <MealCal>{meal?.cal}</MealCal>
         </Row>
       </MealHeader>
+      <MealBody>
+        <MealContent>{meal?.content}</MealContent>
+      </MealBody>
     </Container>
   );
 };
 
 const Container = styled.div`
   width: 100%;
-  height: 68vh;
+  height: 56vh;
   border-radius: 12px;
   background-color: ${color.white};
-  margin: 10px 0;
+  box-shadow: 4px 4px 15px 0 rgba(0, 0, 0, 0.15);
+  margin: 16px 0;
+  transition: ease-in-out;
+  transition-duration: 0.2s;
+
+  &:hover {
+    width: 110%;
+    height: 58vh;
+    transition: ease-in-out;
+    transition-duration: 0.2s;
+  }
 `;
 
 const MealHeader = styled.div`
   width: 100%;
-  height: 9%;
+  height: 32px;
   padding: 0 16px;
   border-radius: 12px 12px 0 0;
   background-color: ${color.meal_header};
-  display: flex;
-  align-items: center;
+  ${flex.CENTER}
+  position: relative;
 `;
 
-const Circle = styled.div`
-  width: 12px;
-  height: 12px;
-  border-radius: 25px;
+const MealTime = styled.span`
+  ${font.p3};
 `;
 
-const Red = styled(Circle)`
-  background-color: ${color.primary_red};
+const MealCal = styled(MealTime)`
+  &:before {
+    content: "· ";
+  }
+
+  &:after {
+    content: "kcal";
+  }
 `;
 
-const Yellow = styled(Circle)`
-  background-color: ${color.primary_yellow};
+const MealBody = styled.div`
+  width: 100%;
+  height: 100%;
+  ${flex.CENTER};
 `;
 
-const Green = styled(Circle)`
-  background-color: ${color.primary_mint};
+const MealContent = styled.p`
+  ${font.p1};
+  line-height: 180%;
+  font-weight: 500;
+  white-space: pre;
+  margin: 14px 0 auto 0;
 `;
 
 export default MealListItem;
