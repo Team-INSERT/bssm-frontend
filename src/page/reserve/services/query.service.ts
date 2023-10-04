5b073df036f6d2ee753d67e37633bb128367097e
Ubinquitous <ubinquitous1@gmail.com>
2023-10-04 19:16:20 +0900

    chore(services): reserve API 정의

---
+++ src/page/reserve/services/query.service.ts
@@ -0,0 +1,14 @@
+import { KEY } from "@/constants";
+import { useQuery } from "@tanstack/react-query";
+import { getReserveList } from "./api.service";
+
+interface IUseReserveListQueryProps {
+  date: string;
+}
+
+export const useReserveListQuery = ({ date }: IUseReserveListQueryProps) => {
+  const { data, ...queryRest } = useQuery([KEY.RESERVE], async () =>
+    getReserveList(date),
+  );
+  return { data, ...queryRest };
+};
