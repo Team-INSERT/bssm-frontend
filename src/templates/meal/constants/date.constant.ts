1481994b60f6e75ad9ba3580b423df7e8458248b
Ubinquitous <ubinquitous1@gmail.com>
2023-11-14 20:48:25 +0900

    chore : 급식 핸들링 관련 날짜 정의

---
+++ src/templates/meal/constants/date.constant.ts
@@ -0,0 +1,13 @@
+const DATE = {
+  FRI: "FRI",
+  MON: "MON",
+  YYMMDD: "YYMMDD",
+  YYYYMMDD: "YYYYMMDD",
+  DETAIL: "YYYY년 M월 D일 dddd",
+  WEEKDAY: "ddd",
+  DAY: "day",
+  INCREASE: "INCREASE",
+  DECREASE: "DECREASE",
+} as const;
+
+export default DATE;
