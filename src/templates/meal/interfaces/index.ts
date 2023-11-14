f5f8f05bfe469a64dc77da3adc58cf19abd6ff2b
Ubinquitous <ubinquitous1@gmail.com>
2023-11-14 20:49:57 +0900

    chore : Meal 아이템에 들어가는 props 타입 정의

---
+++ src/templates/meal/interfaces/index.ts
@@ -0,0 +1,2 @@
+export type { default as MealArrowIconProps } from "./@props/mealArrowIconProps.interface";
+export type { default as MealListItemProps } from "./@props/mealListItemProps.interface";
