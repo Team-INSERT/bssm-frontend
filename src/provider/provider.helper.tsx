29926f89460f1e8e5dd8a4fff97f60aa8afbe799
Ubinquitous <ubinquitous1@gmail.com>
2023-11-15 11:43:46 +0900

    chore : 아폴로 클라이언트 삭제

--- src/provider/provider.helper.tsx
+++ src/provider/provider.helper.tsx
@@ -4,18 +4,15 @@ import React from "react";
 import { RecoilRoot } from "recoil";
 import ReactQueryProvider from "./reactQueryProvider.helper";
 import LayoutProvider from "./layoutProvider.helper";
-import ApolloClientProvider from "./apolloClientProvider.helper";
 import StyledComponentsProvider from "./styledComponentsProvider.helper";
 
 const Provider = ({ children }: React.PropsWithChildren) => {
   return (
     <StyledComponentsProvider>
       <ReactQueryProvider>
-        <ApolloClientProvider>
-          <RecoilRoot>
-            <LayoutProvider>{children}</LayoutProvider>
-          </RecoilRoot>
-        </ApolloClientProvider>
+        <RecoilRoot>
+          <LayoutProvider>{children}</LayoutProvider>
+        </RecoilRoot>
       </ReactQueryProvider>
     </StyledComponentsProvider>
   );
