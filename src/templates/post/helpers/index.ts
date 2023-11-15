de0d9bdc1f24b8e129906990d99e92ee97f9ceba
Ubinquitous <ubinquitous1@gmail.com>
2023-11-15 12:23:06 +0900

    fix(post): helpers 파일명 수정

--- src/templates/post/helpers/index.ts
+++ src/templates/post/helpers/index.ts
@@ -1,2 +1,2 @@
 export { default as get회당불러올게시글개수 } from "./get회당불러올게시글개수.helper";
-export { default as get카테고리명ByCategory } from "./get카테고리명ByCategory";
+export { default as get카테고리명ByCategory } from "./get카테고리명ByCategory.helper";
