8a0546375f71c30042fff1aa3bc33cd492924f77
Ubinquitous <ubinquitous1@gmail.com>
2023-11-16 14:05:40 +0900

    refactor(calender): 커스텀 훅으로 state 정리

---
+++ src/templates/calender/hooks/useCalender.tsx
@@ -0,0 +1,93 @@
+import DATE from "@/constants/date.constant";
+import dayjs from "dayjs";
+import React from "react";
+import useModal from "@/hooks/useModal";
+import useUser from "@/hooks/useUser";
+import { toast } from "react-toastify";
+import {
+  useAddCalenderPlanMutation,
+  useDeleteCalenderPlanMutation,
+} from "../services/mutation.service";
+import CalenderPlanAddModal from "../layouts/CalenderPlanAddModal";
+import { ChangeDateType } from "../types";
+import { CalenderPlanAddQuery } from "../interfaces";
+import { getPlanTypeByPlanName } from "../helpers";
+
+const useCalender = () => {
+  const [currentMonth, setCurrentMonth] = React.useState(dayjs().month());
+  const { openModal } = useModal();
+  const { user } = useUser();
+  const { mutate: deleteMutate } = useDeleteCalenderPlanMutation();
+  const { mutate: addPlanMutate } = useAddCalenderPlanMutation();
+
+  const firstDayOfMonth = () => {
+    const dayOfWeek = dayjs().year(dayjs().year()).month(currentMonth).date(1);
+    return Array.from({ length: dayOfWeek.day() });
+  };
+
+  const isPlanWriterIsUser = (id: number) => {
+    return user.id === id;
+  };
+
+  const handleOpenModalClick = (date?: string) => {
+    if (date) {
+      openModal({
+        component: <CalenderPlanAddModal date={date} />,
+      });
+    }
+  };
+
+  const handlePlanAddButtonClick = ({
+    title,
+    date,
+    planType,
+  }: CalenderPlanAddQuery) => {
+    if (title.trim()) return toast.error("내용을 입력해주세요!");
+    const { grade, classNum: classNumber } = user;
+    const planQuery = {
+      title,
+      date,
+      type: getPlanTypeByPlanName(planType),
+      priority: 0,
+      color: "#000",
+      grade,
+      classNumber,
+    };
+    addPlanMutate(planQuery);
+  };
+
+  const handleDeleteCalenderPlanClick = (id: number) => {
+    deleteMutate(id);
+  };
+
+  const handleCalenderMonthChange = (month: ChangeDateType) => {
+    if (month === DATE.INCREASE) {
+      setCurrentMonth((prevMon) => (prevMon !== 12 ? prevMon + 1 : prevMon));
+    }
+    if (month === DATE.DECREASE) {
+      setCurrentMonth((prevMon) => (prevMon !== 1 ? prevMon - 1 : prevMon));
+    }
+  };
+
+  React.useEffect(() => {
+    const handleSetDateKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "ArrowLeft") handleCalenderMonthChange(DATE.DECREASE);
+      if (e.key === "ArrowRight") handleCalenderMonthChange(DATE.INCREASE);
+    };
+
+    window.addEventListener("keydown", handleSetDateKeyDown);
+    return () => window.removeEventListener("keydown", handleSetDateKeyDown);
+  }, []);
+
+  return {
+    currentMonth,
+    isPlanWriterIsUser,
+    handleOpenModalClick,
+    handlePlanAddButtonClick,
+    handleDeleteCalenderPlanClick,
+    handleCalenderMonthChange,
+    firstDayOfMonth,
+  };
+};
+
+export default useCalender;
