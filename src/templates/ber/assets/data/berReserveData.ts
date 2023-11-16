f4d818b2a9ced70cecc0c5221173e54208729d31
Ubinquitous <ubinquitous1@gmail.com>
2023-11-17 00:11:12 +0900

    refactor(ber): default data 모듈화

---
+++ src/templates/ber/assets/data/berReserveData.ts
@@ -0,0 +1,22 @@
+const berReserveData = {
+  reservedBerNumber: [],
+  berResList: [
+    {
+      id: 0,
+      berNumber: 0,
+      reservation: "2000-03-01",
+      reservationUsersName: "",
+      user: {
+        id: 0,
+        name: "",
+        nickname: "",
+        profileImage: "",
+        grade: 0,
+        class_number: 0,
+        student_number: 0,
+      },
+    },
+  ],
+};
+
+export default berReserveData;
