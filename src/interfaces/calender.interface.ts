e52709054b99f1697721ab967fc29210dd66349a
Ubinquitous <ubinquitous1@gmail.com>
2023-11-16 14:02:42 +0900

    refactor(constant): date, direction 글로벌로 추가

--- src/interfaces/calender.interface.ts
+++
@@ -1,9 +0,0 @@
-export default interface ICalender {
-  title: string;
-  priority: number;
-  date: string;
-  color: string;
-  type: string;
-  grade: number;
-  classNumber: number;
-}
--- src/interfaces/calenderItem.interface.ts
+++
@@ -1,6 +0,0 @@
-import IPlan from "./plan.interface";
-
-export default interface ICalenderItem {
-  date: string;
-  plans: Array<IPlan>;
-}
