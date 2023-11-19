652572efc3307dcf1ab5fe8537e50e8a2ecb49c8
Ubinquitous <ubinquitous1@gmail.com>
2023-11-19 14:00:59 +0900

    refactor(atoms): 버튼 정렬 지정해주는 props 추가

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,21 +1,44 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { color, font } from "@/styles";
 
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color: string;
+  align?: "TOP" | "RIGHT" | "BOTTOM" | "LEFT";
 }
 
 const Button = ({ ...props }: IButtonProps) => {
   return <StyledButton {...props} />;
 };
 
-const StyledButton = styled.button<{ color: string }>`
+const StyledButton = styled.button<{ color: string; align?: string }>`
   width: fit-content;
   padding: 4px 16px;
   border-radius: 4px;
   background-color: ${(props) => props.color};
   color: ${color.white};
   ${font.btn3};
+  ${({ align }) => {
+    switch (align) {
+      case "TOP":
+        return css`
+          margin-bottom: auto;
+        `;
+      case "RIGHT":
+        return css`
+          margin-left: auto;
+        `;
+      case "BOTTOM":
+        return css`
+          margin-top: auto;
+        `;
+      case "LEFT":
+        return css`
+          margin-right: auto;
+        `;
+      default:
+        return "";
+    }
+  }}
 `;
 
 export default Button;
