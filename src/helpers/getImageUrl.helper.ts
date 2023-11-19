01b346dd5ab8b7b18824af477035e7ea7ce9e9a2
Ubinquitous <ubinquitous1@gmail.com>
2023-11-19 14:02:48 +0900

    refactor(atoms): ? 키워드로 undefined 대신하기

--- src/helpers/getImageUrl.helper.ts
+++ src/helpers/getImageUrl.helper.ts
@@ -1,6 +1,6 @@
 import httpClient from "@/apis/httpClient";
 
-const getImageUrl = async (file: File | undefined) => {
+const getImageUrl = async (file?: File) => {
   if (file) {
     const formData = new FormData();
     formData.append("image", file, file.name);
