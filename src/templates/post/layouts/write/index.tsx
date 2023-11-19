ebbc3eda018594c26c65c912628bfcd181acdb61
Ubinquitous <ubinquitous1@gmail.com>
2023-11-19 14:10:43 +0900

    chore(post): 게시글 작성 컴포넌트 정의

---
+++ src/templates/post/layouts/write/index.tsx
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { color, flex } from "@/styles";
+import { Button } from "@/components/atoms";
+import { PostPageParams } from "../../interfaces";
+import { usePostQuery } from "../../services/query.service";
+import {
+  TitleInputBox,
+  ProjectInputBox,
+  CodeReviewInputBox,
+  LostFoundInputBox,
+  FoundInputBox,
+  ContentInputBox,
+} from "../components";
+import { CATEGORY } from "../../constants";
+import { usePostWritable } from "../../hooks";
+import PostCategoryBox from "./CategoryBox";
+import WriteTitleBox from "./WriteTitleBox";
+
+const PostWritePage = ({ id }: PostPageParams) => {
+  const { post } = usePostQuery(id);
+  const {
+    postData,
+    lostImageUrl,
+    handleCategoryChangeClick,
+    handleImageFileSelect,
+    handlePostWriteButtonClick,
+    handleInputPostDataChange,
+  } = usePostWritable(post);
+  return (
+    <Layout>
+      <WriteTitleBox />
+      <PostCategoryBox
+        handleChangeCategory={handleCategoryChangeClick}
+        currentCategory={postData.category}
+      />
+      <InputBox>
+        <TitleInputBox
+          handleChange={handleInputPostDataChange}
+          postData={postData}
+        />
+        {postData.category === CATEGORY.PROJECT && (
+          <ProjectInputBox
+            handleChange={handleInputPostDataChange}
+            postData={postData}
+          />
+        )}
+        {postData.category === CATEGORY.CODE_REVIEW && (
+          <CodeReviewInputBox
+            handleChange={handleInputPostDataChange}
+            postData={postData}
+          />
+        )}
+        {(postData.category === CATEGORY.LOST ||
+          postData.category === CATEGORY.FOUND) && (
+          <LostFoundInputBox
+            handleChange={handleInputPostDataChange}
+            handleFileSelect={handleImageFileSelect}
+            postData={postData}
+            lostImageUrl={lostImageUrl}
+          />
+        )}
+        {postData.category === CATEGORY.FOUND && (
+          <FoundInputBox
+            handleChange={handleInputPostDataChange}
+            postData={postData}
+          />
+        )}
+        <ContentInputBox
+          handleChange={handleInputPostDataChange}
+          postData={postData}
+        />
+        <Button
+          align="RIGHT"
+          color={color.primary_blue}
+          onClick={handlePostWriteButtonClick}
+        >
+          작성
+        </Button>
+      </InputBox>
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  width: 100%;
+  ${flex.COLUMN};
+  gap: 16px;
+`;
+
+const InputBox = styled.div`
+  ${flex.COLUMN};
+  gap: 18px;
+`;
+
+export default PostWritePage;
