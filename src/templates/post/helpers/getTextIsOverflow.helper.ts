0a2c2e0a062f13b0bb068a1598eeb379f979f6f6
Ubinquitous <ubinquitous1@gmail.com>
2023-11-20 02:37:16 +0900

    chore(post): getTextIsOverflow 헬퍼 정의

---
+++ src/templates/post/helpers/getTextIsOverflow.helper.ts
@@ -0,0 +1,12 @@
+const getTextIsOverflow = (content: string) => {
+  const sentences = content.split("\n");
+  const depth = sentences.length;
+
+  if (depth > 4) {
+    const summaryContent = sentences.slice(0, 4).join("\n");
+    return `${summaryContent}...`;
+  }
+  return content;
+};
+
+export default getTextIsOverflow;
