80cd8b8a883a0ce3de5c5ba6f3680220b015a956
Ubinquitous <ubinquitous1@gmail.com>
2023-11-20 02:37:27 +0900

    chore(post): helpers index export 추가

--- src/templates/post/helpers/index.ts
+++ src/templates/post/helpers/index.ts
@@ -3,3 +3,5 @@ export { default as get카테고리명ByCategory } from "./get카테고리명ByC
 export { default as getFilteredPostDataByCategory } from "./getFilteredPostDataByCategory.helper";
 export { default as getPostIsValid } from "./getPostIsValid.helper";
 export { default as getS3ImageUrl } from "./getS3ImageUrl.helper";
+export { default as getTextIsOverflow } from "./getTextIsOverflow.helper";
+export { default as getTextDepthCount } from "./getTextDepthCount.helper";
