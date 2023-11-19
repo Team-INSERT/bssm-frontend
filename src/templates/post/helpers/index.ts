e6cfb46eaf1b48a8afb86a40f730f56ba02baa8a
Ubinquitous <ubinquitous1@gmail.com>
2023-11-19 14:05:57 +0900

    chore(post): export default 추가

--- src/templates/post/helpers/index.ts
+++ src/templates/post/helpers/index.ts
@@ -1,2 +1,5 @@
 export { default as get회당불러올게시글개수 } from "./get회당불러올게시글개수.helper";
 export { default as get카테고리명ByCategory } from "./get카테고리명ByCategory.helper";
+export { default as getFilteredPostDataByCategory } from "./getFilteredPostDataByCategory.helper";
+export { default as getPostIsValid } from "./getPostIsValid.helper";
+export { default as getS3ImageUrl } from "./getS3ImageUrl.helper";
--- src/templates/post/hooks/index.ts
+++ src/templates/post/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as usePost } from "./usePost";
+export { default as usePostWritable } from "./usePostWritable";
