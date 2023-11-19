a14ef5c7b85e8290aaecf38d25b3a46dcce962fc
Ubinquitous <ubinquitous1@gmail.com>
2023-11-20 02:34:16 +0900

    refactor(constants): 전역 constants 모두 제거

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,3 @@
-export { default as APPLICATIONS } from "./applications.constant";
 export { default as ERROR } from "./error.constant";
 export { default as KEY } from "./key.constant";
-export { default as LOSTFOUND } from "./lostfound.constant";
 export { default as ROUTER } from "./router.constant";
-export { default as SERVICE } from "./service.constant";
-export { default as USER } from "./user.constant";
-export { default as LIKE } from "./likeType.constant";
