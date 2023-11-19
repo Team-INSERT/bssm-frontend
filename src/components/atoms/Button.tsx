06e00f3ea4c746deee07c89b930fe3187433291b
Ubinquitous <ubinquitous1@gmail.com>
2023-11-20 02:33:30 +0900

    refactor(atoms): button size props 추가

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -4,19 +4,32 @@ import { color, font } from "@/styles";
 interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   color: string;
   align?: "TOP" | "RIGHT" | "BOTTOM" | "LEFT";
+  isSmall?: boolean;
 }
 
 const Button = ({ ...props }: IButtonProps) => {
   return <StyledButton {...props} />;
 };
 
-const StyledButton = styled.button<{ color: string; align?: string }>`
+const StyledButton = styled.button<{
+  color: string;
+  align?: string;
+  isSmall?: boolean;
+}>`
   width: fit-content;
-  padding: 4px 16px;
   border-radius: 4px;
   background-color: ${(props) => props.color};
   color: ${color.white};
-  ${font.btn3};
+  ${({ isSmall }) =>
+    isSmall
+      ? css`
+          padding: 3px 10px;
+          ${font.caption};
+        `
+      : css`
+          padding: 4px 16px;
+          ${font.btn3};
+        `};
   ${({ align }) => {
     switch (align) {
       case "TOP":
