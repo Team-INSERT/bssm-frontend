eb3f007a2c3d0da1474b67459a381d595b223ad4
Ubinquitous <ubinquitous1@gmail.com>
2023-11-20 16:51:15 +0900

    chore(meister): useMeister 훅 정의

---
+++ src/templates/meister/hooks/useMeister.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import useModal from "@/hooks/useModal";
+import LoginModal from "@/components/common/Modal/LoginModal";
+import {
+  useMeisterDetailQuery,
+  useMeisterQuery,
+} from "../services/query.service";
+import meisterDefaultData from "../assets/data/meisterDefaultData";
+import { getStudentInformationHTML } from "../helpers";
+import meisterDefaultDetailData from "../assets/data/meisterDetailDefaultData";
+
+const useMeister = () => {
+  const { openModal } = useModal();
+  const meisterQuery = useMeisterQuery();
+  const [studentNum, setStudentNum] = React.useState("");
+  const meisterDetailQuery = useMeisterDetailQuery(studentNum);
+  const [meister, setMeister] = React.useState(meisterDefaultData);
+  const [meisterDetail, setMeisterDetail] = React.useState(
+    meisterDefaultDetailData,
+  );
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [studentInfo, setStudentInfo] = React.useState("");
+
+  const handleStudentSearchClick = () => {
+    setIsLoading(true);
+    meisterDetailQuery.refetch().then(() => setIsLoading(false));
+  };
+
+  const handleStudentNumChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const [grade, classNum, studentNum1, studentNum2] = [
+      +value[0],
+      +value[1],
+      +value[2],
+      +value[3],
+    ];
+    const studentNumber = `${studentNum1}${studentNum2}`;
+    if (value.length > 4) return;
+    if (Number.isNaN(+value)) return;
+    if (grade > 3 || grade === 0) return;
+    if (classNum > 4 || classNum === 0) return;
+    if (studentNum1 > 1) return;
+    if (+studentNumber > 16 || +studentNumber === 0) return;
+    setStudentNum(value);
+  };
+
+  React.useEffect(() => {
+    if (meisterDetailQuery.isSuccess) {
+      setMeisterDetail(meisterDetailQuery.data);
+      const { scoreHtmlContent } = meisterDetailQuery.data.meister;
+      setStudentInfo(getStudentInformationHTML(scoreHtmlContent));
+    }
+  }, [meisterDetailQuery]);
+
+  React.useEffect(() => {
+    if (meisterQuery.isSuccess) setMeister(meisterQuery.data);
+    if (meisterQuery.isError) openModal({ component: <>asd</> });
+  }, [meisterQuery]);
+
+  return {
+    isLoading,
+    isSuccess: meisterDetailQuery.isSuccess,
+    meister,
+    meisterDetail,
+    studentInfo,
+    studentNum,
+    handleStudentNumChange,
+    handleStudentSearchClick,
+  };
+};
+
+export default useMeister;
