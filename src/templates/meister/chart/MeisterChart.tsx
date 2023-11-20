7d59b2ecfd21e2305efe87fc0e593f32b492b099
Ubinquitous <ubinquitous1@gmail.com>
2023-11-20 16:49:55 +0900

    chore(meister): MeisterChart

--- src/templates/meister/chart/MeisterChart.tsx
+++ src/templates/meister/chart/MeisterChart.tsx
@@ -1,63 +1,15 @@
 import React, { useState } from "react";
 import * as d3 from "d3";
 import { color } from "@/styles";
-import { IMeister } from "@/interfaces";
 import { useDidMountEffect } from "@/hooks/useDidMountEffect";
+import { Meister } from "../interfaces";
+import {
+  graphColorData,
+  meisterChartData,
+  meisterVariables,
+} from "../assets/data";
 
-interface MeisterChartData {
-  group:
-    | "basicJobSkills"
-    | "professionalTech"
-    | "workEthic"
-    | "humanities"
-    | "foreignScore";
-  scoreType: "avg" | "meister" | "max";
-}
-
-const graphColor = {
-  avg: color.primary_yellow,
-  meister: color.primary_blue,
-  max: color.primary_red,
-};
-
-const groups = [
-  "직업 기초 능력",
-  "전문 기술 역량",
-  "인성 직업 의식",
-  "인문학적 소양",
-  "외국어 능력",
-];
-
-const data: MeisterChartData[] = [
-  { group: "basicJobSkills", scoreType: "avg" },
-  { group: "basicJobSkills", scoreType: "meister" },
-  { group: "basicJobSkills", scoreType: "max" },
-
-  { group: "professionalTech", scoreType: "avg" },
-  { group: "professionalTech", scoreType: "meister" },
-  { group: "professionalTech", scoreType: "max" },
-
-  { group: "workEthic", scoreType: "avg" },
-  { group: "workEthic", scoreType: "meister" },
-  { group: "workEthic", scoreType: "max" },
-
-  { group: "humanities", scoreType: "avg" },
-  { group: "humanities", scoreType: "meister" },
-  { group: "humanities", scoreType: "max" },
-
-  { group: "foreignScore", scoreType: "avg" },
-  { group: "foreignScore", scoreType: "meister" },
-  { group: "foreignScore", scoreType: "max" },
-];
-
-const meisterSeparation = [0, 30, 60];
-
-interface IMeisterChartProps {
-  meisterData: IMeister;
-}
-
-const MeisterChart = ({ meisterData }: IMeisterChartProps) => {
-  // const canvasRef = React.useRef(null);
+const MeisterChart = ({ ...meisterData }: Meister) => {
   const [canvasRef, setCanvasRef] = useState<HTMLDivElement | null>(null);
 
   useDidMountEffect(() => {
@@ -68,30 +20,27 @@ const MeisterChart = ({ meisterData }: IMeisterChartProps) => {
       .append("svg")
       .attr("width", "100%")
       .attr("height", "14vw");
-
     let separator = 0;
-
-    data.forEach((item, idx) => {
+    meisterChartData.forEach((item, idx) => {
       const index = idx + 1;
       const startX = 1.5;
 
       const X = `${index * 4 + startX + separator}%`;
 
       if (index % 3 === 0) separator += 8;
-
+      const rectangleHeight =
+        (meisterData[item.scoreType][item.group] / meisterChartData.length) *
+        120;
       svg
         .append("rect")
         .attr("x", X)
         .attr("height", "0%")
         .attr("y", "100%")
         .attr("width", 15)
-        .attr("fill", graphColor[item.scoreType])
+        .attr("fill", graphColorData[item.scoreType])
         .transition()
         .duration(1000)
-        .attr(
-          "height",
-          `${(meisterData[item.scoreType][item.group] / data.length) * 120}%`,
-        )
+        .attr("height", `${rectangleHeight}%`)
         .attr("y", `${110 - meisterData[item.scoreType][item.group]}%`);
 
       svg
@@ -108,7 +57,7 @@ const MeisterChart = ({ meisterData }: IMeisterChartProps) => {
         .style("fill", color.gray);
     });
 
-    groups.forEach((groupText, index) => {
+    meisterVariables.forEach((groupText, index) => {
       svg
         .append("text")
         .attr("x", `${(index + 1) * 19.5 - 12}%`)
@@ -119,7 +68,7 @@ const MeisterChart = ({ meisterData }: IMeisterChartProps) => {
         .style("font-size", "12px");
     });
 
-    meisterSeparation.forEach((item) => {
+    [0, 30, 60].forEach((item) => {
       svg
         .append("line")
         .attr("x1", 40)
@@ -139,7 +88,7 @@ const MeisterChart = ({ meisterData }: IMeisterChartProps) => {
         .style("font-weight", "600")
         .style("font-size", "12px");
     });
-  }, [meisterData, canvasRef]);
+  }, [canvasRef]);
 
   return <div ref={setCanvasRef} />;
 };
