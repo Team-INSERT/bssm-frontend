5254c05f525bf50b8bbbc4e8359fff54f6ce1dcb
Ubinquitous <ubinquitous1@gmail.com>
2023-11-22 07:42:35 +0900

    refactor(constant): 상수 추가 및 삭제

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,4 @@
-export { default as ERROR } from "./error.constant";
+export { default as DATE } from "./date.constant";
+export { default as DIRECTION } from "./direction.constant";
 export { default as KEY } from "./key.constant";
 export { default as ROUTER } from "./router.constant";
