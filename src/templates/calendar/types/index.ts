3c0f79f413419f811cf03f3e312470c45f4074c8
Ubinquitous <ubinquitous1@gmail.com>
2023-11-22 07:54:31 +0900

    fix(calendar) : 대소문자 이슈

--- src/templates/calendar/types/index.ts
+++ src/templates/calendar/types/index.ts
@@ -1,4 +1,4 @@
 export type { default as CalendarItem } from "./CalendarItem.type";
 export type { default as Calendar } from "./Calender.type";
 export type { default as CalendarPlan } from "./CalendarPlan.type";
-export type { default as CalendarArrowDirectionType } from "./AlendarArrowDirection.type";
+export type { default as CalendarArrowDirectionType } from "./CalendarArrowDirection.type";
