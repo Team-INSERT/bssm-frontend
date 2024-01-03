8814fb94785a8f66d8bcbd8d540aad8b19f523dc
youngeunkim <pkhj286@gmail.com>
2024-01-03 14:09:19 +0900

    style: css

--- src/components/common/Header/@setting/layouts/setting.tsx
+++ src/components/common/Header/@setting/layouts/setting.tsx
@@ -10,7 +10,7 @@ interface SettingModalProps {
 
 const SettingModal = ({ onClose }: SettingModalProps) => {
   return (
-    <ModalOverlay>
+    <Modal>
       <ModalContent>
         <SettingBox>
           <SettingText>설정</SettingText>
@@ -20,18 +20,15 @@ const SettingModal = ({ onClose }: SettingModalProps) => {
         </SettingBox>
         <LogoutBox>로그아웃</LogoutBox>
       </ModalContent>
-    </ModalOverlay>
+    </Modal>
   );
 };
 
-const ModalOverlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
+const Modal = styled.div`
   ${flex.CENTER};
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.5);
+  background: #00000080;
 `;
 
 const ModalContent = styled.div`
@@ -43,22 +40,29 @@ const ModalContent = styled.div`
 const SettingBox = styled.div`
   ${flex.VERTICAL}
 `;
+
 const SettingText = styled.p`
   ${flex.CENTER};
   ${font.H4};
 `;
+
 const ExitButton = styled(Image)`
   margin-left: 17rem;
 `;
+
 const LogoutBox = styled.div`
   ${flex.CENTER};
   margin: 7% 0 0 0;
+  padding: 2%;
   border-radius: 5%;
+  background-color: #f1f3f5;
   color: ${theme.black};
   ${font.H4};
-  background-color: #f1f3f5;
-  padding: 2%;
   cursor: pointer;
+
+  &:hover {
+    background-color: ${theme.content};
+  }
 `;
 
 export default SettingModal;
